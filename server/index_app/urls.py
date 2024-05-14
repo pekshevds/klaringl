8262b265bae73874e0adcfc48277550c114bfc00
denis sergeevich pekshev <pekshev.ds@gmail.com>
2024-05-14 15:56:20 +0300

    routing

--- server/index_app/urls.py
+++ server/index_app/urls.py
@@ -1,8 +1,14 @@
 from django.urls import path
-from index_app.views import IndexView
+from index_app.views import (
+    IndexView,
+    AboutView,
+    ContactView
+)
 
 app_name = "index"
 
 urlpatterns = [
     path('', IndexView.as_view(), name="index"),
+    path('about/', AboutView.as_view(), name="about"),
+    path('contact/', ContactView.as_view(), name="contact"),
 ]
--- server/index_app/views.py
+++ server/index_app/views.py
@@ -6,5 +6,19 @@ from django.views.generic import View
 class IndexView(View):
     def get(self, request: HttpRequest) -> HttpResponse:
         return render(request,
-                      "index_app/index.html.html",
+                      "index_app/index.html.htm",
+                      {})
+
+
+class AboutView(View):
+    def get(self, request: HttpRequest) -> HttpResponse:
+        return render(request,
+                      "index_app/about.html.htm",
+                      {})
+
+
+class ContactView(View):
+    def get(self, request: HttpRequest) -> HttpResponse:
+        return render(request,
+                      "index_app/contact.html.htm",
                       {})
