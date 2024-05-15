a8f0e05d96a07853ad1e9bdd361246ca0694c16e
MaximPekshev <mp198769@gmail.com>
2024-05-15 12:53:31 +0600

    layout bug fix

--- server/index_app/views.py
+++ server/index_app/views.py
@@ -7,21 +7,21 @@ from index_app.forms import GetInTouchForm
 class IndexView(View):
     def get(self, request: HttpRequest) -> HttpResponse:
         return render(request,
-                      "index_app/index.html.htm",
+                      "index_app/index.html",
                       {})
 
 
 class AboutView(View):
     def get(self, request: HttpRequest) -> HttpResponse:
         return render(request,
-                      "index_app/about.html.htm",
+                      "index_app/about.html",
                       {})
 
 
 class ContactView(View):
     def get(self, request: HttpRequest) -> HttpResponse:
         return render(request,
-                      "index_app/contact.html.htm",
+                      "index_app/contact.html",
                       {})
 
 
@@ -31,5 +31,5 @@ class GetInTouchView(View):
         if form.is_valid():
             print(form.cleaned_data)
         return render(request,
-                      "index_app/contact.html.htm",
+                      "index_app/contact.html",
                       {})
