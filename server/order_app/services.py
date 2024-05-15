040f56d46ec20e059fcbffac646d85ac4001969d
pekshevds <pekshev.ds@gmail.com>
2024-05-15 18:41:08 +0300

    calculate rate

--- server/order_app/services.py
+++ server/order_app/services.py
@@ -21,57 +21,92 @@ def rate_by_cities(city_from: City, city_to: City) -> Rate | None:
     return rate
 
 
+def calculate_cost_by_weight(weight: float, rate: Rate) -> float:
+    cost_by_weight = 0
+    if 0 < weight <= rate.cost_by_weight_0_25:
+        cost_by_weight = rate.cost_by_weight_0_25
+
+    if rate.cost_by_weight_0_25 < weight <= rate.cost_by_weight_25_50:
+        cost_by_weight = rate.cost_by_weight_25_50
+
+    if rate.cost_by_weight_25_50 < weight <= rate.cost_by_weight_50_150:
+        cost_by_weight = rate.cost_by_weight_50_150 * weight
+
+    if rate.cost_by_weight_50_150 < weight <= rate.cost_by_weight_150_300:
+        cost_by_weight = rate.cost_by_weight_150_300 * weight
+
+    if rate.cost_by_weight_150_300 < weight <= rate.cost_by_weight_300_500:
+        cost_by_weight = rate.cost_by_weight_300_500 * weight
+
+    if rate.cost_by_weight_300_500 < weight <= rate.cost_by_weight_500_1000:
+        cost_by_weight = rate.cost_by_weight_500_1000 * weight
+
+    if rate.cost_by_weight_500_1000 < weight <= rate.cost_by_weight_1000_1500:
+        cost_by_weight = rate.cost_by_weight_1000_1500 * weight
+
+    if rate.cost_by_weight_1000_1500 < weight <= rate.cost_by_weight_1500_2000:
+        cost_by_weight = rate.cost_by_weight_1500_2000 * weight
+
+    if rate.cost_by_weight_1500_2000 < weight <= rate.cost_by_weight_2000_3000:
+        cost_by_weight = rate.cost_by_weight_2000_3000 * weight
+
+    if rate.cost_by_weight_2000_3000 < weight <= rate.cost_by_weight_3000_5000:
+        cost_by_weight = rate.cost_by_weight_3000_5000 * weight
+
+    if rate.cost_by_weight_3000_5000 < weight\
+            <= rate.cost_by_weight_5000_10000:
+        cost_by_weight = rate.cost_by_weight_5000_10000 * weight
+
+    if weight > rate.cost_by_weight_10000_inf:
+        cost_by_weight = rate.cost_by_weight_10000_inf * weight
+    return cost_by_weight
+
+
+def calculate_cost_by_volume(volume: float, rate: Rate) -> float:
+    cost_by_volume = 0
+    if 0 < volume <= rate.cost_by_volume_0_01:
+        cost_by_volume = rate.cost_by_volume_0_01 * volume
+
+    if rate.cost_by_volume_0_01 < volume <= rate.cost_by_volume_01_02:
+        cost_by_volume = rate.cost_by_volume_01_02 * volume
+
+    if rate.cost_by_volume_01_02 < volume <= rate.cost_by_volume_02_06:
+        cost_by_volume = rate.cost_by_volume_02_06 * volume
+
+    if rate.cost_by_volume_02_06 < volume <= rate.cost_by_volume_06_12:
+        cost_by_volume = rate.cost_by_volume_06_12 * volume
+
+    if rate.cost_by_volume_06_12 < volume <= rate.cost_by_volume_12_20:
+        cost_by_volume = rate.cost_by_volume_12_20 * volume
+
+    if rate.cost_by_volume_12_20 < volume <= rate.cost_by_volume_20_40:
+        cost_by_volume = rate.cost_by_volume_20_40 * volume
+
+    if rate.cost_by_volume_20_40 < volume <= rate.cost_by_volume_40_60:
+        cost_by_volume = rate.cost_by_volume_40_60 * volume
+
+    if rate.cost_by_volume_40_60 < volume <= rate.cost_by_volume_60_80:
+        cost_by_volume = rate.cost_by_volume_60_80 * volume
+
+    if rate.cost_by_volume_60_80 < volume <= rate.cost_by_volume_80_120:
+        cost_by_volume = rate.cost_by_volume_80_120 * volume
+
+    if rate.cost_by_volume_80_120 < volume <= rate.cost_by_volume_120_200:
+        cost_by_volume = rate.cost_by_volume_120_200 * volume
+
+    if rate.cost_by_volume_120_200 < volume <= rate.cost_by_volume_200_400:
+        cost_by_volume = rate.cost_by_volume_200_400 * volume
+
+    if volume > rate.cost_by_volume_400_inf:
+        cost_by_volume = rate.cost_by_volume_400_inf * volume
+    return cost_by_volume
+
+
 def calculate_delivery_cost(
         rate: Rate,
         weight: float = 0, volume: float = 0) -> float:
 
-    cost_by_weight = 0
-    if weight > rate.cost_by_weight_10000_inf:
-        cost_by_weight = rate.cost_by_weight_10000_inf
-
-    if weight > 0 and weight <= rate.cost_by_weight_0_25:
-        cost_by_weight = rate.cost_by_weight_0_25
-
-    if weight > rate.cost_by_weight_0_25 and\
-            weight <= rate.cost_by_weight_25_50:
-        cost_by_weight = rate.cost_by_weight_25_50
-
-    if weight > rate.cost_by_weight_25_50 and\
-            weight <= rate.cost_by_weight_50_150:
-        cost_by_weight = rate.cost_by_weight_50_150
-
-    if weight > rate.cost_by_weight_50_150 and\
-            weight <= rate.cost_by_weight_150_300:
-        cost_by_weight = rate.cost_by_weight_150_300
-
-    if weight > rate.cost_by_weight_150_300 and\
-            weight <= rate.cost_by_weight_300_500:
-        cost_by_weight = rate.cost_by_weight_300_500
-
-    if weight > rate.cost_by_weight_300_500 and\
-            weight <= rate.cost_by_weight_500_1000:
-        cost_by_weight = rate.cost_by_weight_500_1000
-
-    if weight > rate.cost_by_weight_500_1000 and\
-            weight <= rate.cost_by_weight_1000_1500:
-        cost_by_weight = rate.cost_by_weight_1000_1500
-
-    if weight > rate.cost_by_weight_1000_1500 and\
-            weight <= rate.cost_by_weight_1500_2000:
-        cost_by_weight = rate.cost_by_weight_1500_2000
-
-    if weight > rate.cost_by_weight_1500_2000 and\
-            weight <= rate.cost_by_weight_2000_3000:
-        cost_by_weight = rate.cost_by_weight_2000_3000
-
-    if weight > rate.cost_by_weight_2000_3000 and\
-            weight <= rate.cost_by_weight_3000_5000:
-        cost_by_weight = rate.cost_by_weight_3000_5000
-
-    if weight > rate.cost_by_weight_3000_5000 and\
-            weight <= rate.cost_by_weight_5000_10000:
-        cost_by_weight = rate.cost_by_weight_5000_10000
-
-    cost_by_volume = 0
+    cost_by_weight = calculate_cost_by_weight(weight, rate)
+    cost_by_volume = calculate_cost_by_volume(volume, rate)
 
     return max(cost_by_weight, cost_by_volume)
