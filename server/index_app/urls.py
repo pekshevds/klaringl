ba473635770d751d9f57c2afea64afb35e1a262e
denis sergeevich pekshev <pekshev.ds@gmail.com>
2024-05-15 07:44:38 +0300

    base models

--- server/index_app/urls.py
+++ server/index_app/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from index_app.views import (
     IndexView,
     AboutView,
-    ContactView
+    ContactView,
+    GetInTouchView
 )
 
 app_name = "index"
@@ -11,4 +12,5 @@ urlpatterns = [
     path('', IndexView.as_view(), name="index"),
     path('about/', AboutView.as_view(), name="about"),
     path('contact/', ContactView.as_view(), name="contact"),
+    path('get-in-touch/', GetInTouchView.as_view(), name="get-in-touch"),
 ]
--- server/index_app/views.py
+++ server/index_app/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpRequest, HttpResponse
 from django.views.generic import View
+from index_app.forms import GetInTouchForm
 
 
 class IndexView(View):
@@ -19,6 +20,16 @@ class AboutView(View):
 
 class ContactView(View):
     def get(self, request: HttpRequest) -> HttpResponse:
+        return render(request,
+                      "index_app/contact.html.htm",
+                      {})
+
+
+class GetInTouchView(View):
+    def post(self, request: HttpRequest) -> HttpResponse:
+        form = GetInTouchForm(request.POST)
+        if form.is_valid():
+            print(form.cleaned_data)
         return render(request,
                       "index_app/contact.html.htm",
                       {})
