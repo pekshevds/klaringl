e1cb5b9613abfb3f452ca6f4b6fb6506d88a0735
denis sergeevich pekshev <pekshev.ds@gmail.com>
2024-05-30 12:08:36 +0300

    correct for publishing

--- server/server/settings.py
+++ server/server/settings.py
@@ -25,7 +25,13 @@ SECRET_KEY = config.SECRET_KEY
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost',]
+ALLOWED_HOSTS = [
+    'klaringl.annasoft.site',
+    'www.klaringl.annasoft.site',
+    '127.0.0.1',
+    '127.0.0.1',
+    'localhost',
+]
 
 
 # Application definition
