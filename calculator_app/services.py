a1dd1177391860b0682caf57df8bfe4bad7520db
denis sergeevich pekshev <pekshev.ds@gmail.com>
2024-05-31 13:14:59 +0300

    change type of calculators from float into decimal

--- calculator_app/services.py
+++ calculator_app/services.py
@@ -1,3 +1,4 @@
+from decimal import Decimal
 from datetime import time
 from django.shortcuts import get_object_or_404
 from calculator_app.models import (
@@ -18,111 +19,116 @@ def city_by_name(city_name: str) -> City | None:
 def rate_by_cities(city_from: City, city_to: City) -> Rate | None:
     try:
         # rate = get_object_or_404(Rate, city_from=city_from, city_to=city_to)
-        rate = Rate.objects.filter(city_from=city_from, city_to=city_to).first()
+        rate = Rate.objects.filter(
+            city_from=city_from, city_to=city_to
+        ).first()
     except Rate.DoesNotExist:
         rate = None
     return rate
 
 
-def calculate_cost_by_weight(rate: Rate, weight: float) -> float:
+def calculate_cost_by_weight(rate: Rate, weight: Decimal) -> Decimal:
+    docimal_0 = Decimal("0")
     if not rate:
-        return 0
-    cost_by_weight = 0
-    if 0 < weight <= float(rate.cost_by_weight_0_25):
-        cost_by_weight = float(rate.cost_by_weight_0_25)
+        return docimal_0
+    cost_by_weight = docimal_0
+    if docimal_0 < weight <= rate.cost_by_weight_0_25:
+        cost_by_weight = rate.cost_by_weight_0_25
 
-    if float(rate.cost_by_weight_0_25) < weight <= float(rate.cost_by_weight_25_50):
-        cost_by_weight = float(rate.cost_by_weight_25_50)
+    if rate.cost_by_weight_0_25 < weight <= rate.cost_by_weight_25_50:
+        cost_by_weight = rate.cost_by_weight_25_50
 
-    if float(rate.cost_by_weight_25_50) < weight <= float(rate.cost_by_weight_50_150):
-        cost_by_weight = float(rate.cost_by_weight_50_150) * weight
+    if rate.cost_by_weight_25_50 < weight <= rate.cost_by_weight_50_150:
+        cost_by_weight = rate.cost_by_weight_50_150 * weight
 
-    if float(rate.cost_by_weight_50_150) < weight <= float(rate.cost_by_weight_150_300):
-        cost_by_weight = float(rate.cost_by_weight_150_300) * weight
+    if rate.cost_by_weight_50_150 < weight <= rate.cost_by_weight_150_300:
+        cost_by_weight = rate.cost_by_weight_150_300 * weight
 
-    if float(rate.cost_by_weight_150_300) < weight <= float(rate.cost_by_weight_300_500):
-        cost_by_weight = float(rate.cost_by_weight_300_500) * weight
+    if rate.cost_by_weight_150_300 < weight <= rate.cost_by_weight_300_500:
+        cost_by_weight = rate.cost_by_weight_300_500 * weight
 
-    if float(rate.cost_by_weight_300_500) < weight <= float(rate.cost_by_weight_500_1000):
-        cost_by_weight = float(rate.cost_by_weight_500_1000) * weight
+    if rate.cost_by_weight_300_500 < weight <= rate.cost_by_weight_500_1000:
+        cost_by_weight = rate.cost_by_weight_500_1000 * weight
 
-    if float(rate.cost_by_weight_500_1000) < weight <= float(rate.cost_by_weight_1000_1500):
-        cost_by_weight = float(rate.cost_by_weight_1000_1500) * weight
+    if rate.cost_by_weight_500_1000 < weight <= rate.cost_by_weight_1000_1500:
+        cost_by_weight = rate.cost_by_weight_1000_1500 * weight
 
-    if float(rate.cost_by_weight_1000_1500) < weight <= float(rate.cost_by_weight_1500_2000):
-        cost_by_weight = float(rate.cost_by_weight_1500_2000) * weight
+    if rate.cost_by_weight_1000_1500 < weight <= rate.cost_by_weight_1500_2000:
+        cost_by_weight = rate.cost_by_weight_1500_2000 * weight
 
-    if float(rate.cost_by_weight_1500_2000) < weight <= float(rate.cost_by_weight_2000_3000):
-        cost_by_weight = float(rate.cost_by_weight_2000_3000) * weight
+    if rate.cost_by_weight_1500_2000 < weight <= rate.cost_by_weight_2000_3000:
+        cost_by_weight = rate.cost_by_weight_2000_3000 * weight
 
-    if float(rate.cost_by_weight_2000_3000) < weight <= float(rate.cost_by_weight_3000_5000):
-        cost_by_weight = float(rate.cost_by_weight_3000_5000) * weight
+    if rate.cost_by_weight_2000_3000 < weight <= rate.cost_by_weight_3000_5000:
+        cost_by_weight = rate.cost_by_weight_3000_5000 * weight
 
-    if float(rate.cost_by_weight_3000_5000) < weight\
-            <= float(rate.cost_by_weight_5000_10000):
-        cost_by_weight = float(rate.cost_by_weight_5000_10000) * weight
+    if rate.cost_by_weight_3000_5000 < weight\
+            <= rate.cost_by_weight_5000_10000:
+        cost_by_weight = rate.cost_by_weight_5000_10000 * weight
 
-    if weight > float(rate.cost_by_weight_10000_inf):
-        cost_by_weight = float(rate.cost_by_weight_10000_inf) * weight
+    if weight > rate.cost_by_weight_10000_inf:
+        cost_by_weight = rate.cost_by_weight_10000_inf * weight
     return cost_by_weight
 
 
-def calculate_cost_by_volume(rate: Rate, volume: float) -> float:
+def calculate_cost_by_volume(rate: Rate, volume: Decimal) -> Decimal:
+    docimal_0 = Decimal("0")
     if not rate:
-        return 0
-    cost_by_volume = 0
-    if 0 < volume <= float(rate.cost_by_volume_0_01):
-        cost_by_volume = float(rate.cost_by_volume_0_01) * volume
+        return docimal_0
+    cost_by_volume = docimal_0
+    if docimal_0 < volume <= rate.cost_by_volume_0_01:
+        cost_by_volume = rate.cost_by_volume_0_01 * volume
 
-    if float(rate.cost_by_volume_0_01) < volume <= float(rate.cost_by_volume_01_02):
-        cost_by_volume = float(rate.cost_by_volume_01_02) * volume
+    if rate.cost_by_volume_0_01 < volume <= rate.cost_by_volume_01_02:
+        cost_by_volume = rate.cost_by_volume_01_02 * volume
 
-    if float(rate.cost_by_volume_01_02) < volume <= float(rate.cost_by_volume_02_06):
-        cost_by_volume = float(rate.cost_by_volume_02_06) * volume
+    if rate.cost_by_volume_01_02 < volume <= rate.cost_by_volume_02_06:
+        cost_by_volume = rate.cost_by_volume_02_06 * volume
 
-    if float(rate.cost_by_volume_02_06) < volume <= float(rate.cost_by_volume_06_12):
-        cost_by_volume = float(rate.cost_by_volume_06_12) * volume
+    if rate.cost_by_volume_02_06 < volume <= rate.cost_by_volume_06_12:
+        cost_by_volume = rate.cost_by_volume_06_12 * volume
 
-    if float(rate.cost_by_volume_06_12) < volume <= float(rate.cost_by_volume_12_20):
-        cost_by_volume = float(rate.cost_by_volume_12_20) * volume
+    if rate.cost_by_volume_06_12 < volume <= rate.cost_by_volume_12_20:
+        cost_by_volume = rate.cost_by_volume_12_20 * volume
 
-    if float(rate.cost_by_volume_12_20) < volume <= float(rate.cost_by_volume_20_40):
-        cost_by_volume = float(rate.cost_by_volume_20_40) * volume
+    if rate.cost_by_volume_12_20 < volume <= rate.cost_by_volume_20_40:
+        cost_by_volume = rate.cost_by_volume_20_40 * volume
 
-    if float(rate.cost_by_volume_20_40) < volume <= float(rate.cost_by_volume_40_60):
-        cost_by_volume = float(rate.cost_by_volume_40_60) * volume
+    if rate.cost_by_volume_20_40 < volume <= rate.cost_by_volume_40_60:
+        cost_by_volume = rate.cost_by_volume_40_60 * volume
 
-    if float(rate.cost_by_volume_40_60) < volume <= float(rate.cost_by_volume_60_80):
-        cost_by_volume = float(rate.cost_by_volume_60_80) * volume
+    if rate.cost_by_volume_40_60 < volume <= rate.cost_by_volume_60_80:
+        cost_by_volume = rate.cost_by_volume_60_80 * volume
 
-    if float(rate.cost_by_volume_60_80) < volume <= float(rate.cost_by_volume_80_120):
-        cost_by_volume = float(rate.cost_by_volume_80_120) * volume
+    if rate.cost_by_volume_60_80 < volume <= rate.cost_by_volume_80_120:
+        cost_by_volume = rate.cost_by_volume_80_120 * volume
 
-    if float(rate.cost_by_volume_80_120) < volume <= float(rate.cost_by_volume_120_200):
-        cost_by_volume = float(rate.cost_by_volume_120_200) * volume
+    if rate.cost_by_volume_80_120 < volume <= rate.cost_by_volume_120_200:
+        cost_by_volume = rate.cost_by_volume_120_200 * volume
 
-    if float(rate.cost_by_volume_120_200) < volume <= float(rate.cost_by_volume_200_400):
-        cost_by_volume = float(rate.cost_by_volume_200_400) * volume
+    if rate.cost_by_volume_120_200 < volume <= rate.cost_by_volume_200_400:
+        cost_by_volume = rate.cost_by_volume_200_400 * volume
 
-    if volume > float(rate.cost_by_volume_400_inf):
-        cost_by_volume = float(rate.cost_by_volume_400_inf) * volume
+    if volume > rate.cost_by_volume_400_inf:
+        cost_by_volume = rate.cost_by_volume_400_inf * volume
     return cost_by_volume
 
 
 def calculate_delivery_cost_by_rate(
         rate: Rate,
-        weight: float = 0, volume: float = 0) -> float:
+        weight: Decimal = 0,
+        volume: Decimal = 0) -> Decimal:
 
     cost_by_weight = calculate_cost_by_weight(rate, weight)
     cost_by_volume = calculate_cost_by_volume(rate, volume)
-
     return max(cost_by_weight, cost_by_volume)
 
 
 def calculate_delivery_cost(
         city_from_name: str,
         city_to_name: str,
-        weight: float = 0, volume: float = 0) -> float:
+        weight: Decimal = 0,
+        volume: Decimal = 0) -> Decimal:
 
     city_from = city_by_name(city_name=city_from_name)
     city_to = city_by_name(city_name=city_to_name)
@@ -134,7 +140,7 @@ def calculate_delivery_cost(
     return max(cost_by_weight, cost_by_volume)
 
 
-def calculate_delevery_on_time(deliver_time: time) -> float:
+def calculate_delevery_on_time(deliver_time: time) -> Decimal:
     """
     Расчет необходимости и стоимости надбавки за ночную/вечернюю доставку"""
     const = Const.info()
@@ -147,7 +153,7 @@ def calculate_delevery_on_time(deliver_time: time) -> float:
     return const.time_deliver_cost
 
 
-def calculate_warehouse_process(volume: float) -> float:
+def calculate_warehouse_process(volume: Decimal) -> Decimal:
     """
     Складская обработка грузов при местной (областной) доставке тогда
     {const.warehouse_process_cost} руб./куб. м. Минимальная стоимость
@@ -159,28 +165,28 @@ def calculate_warehouse_process(volume: float) -> float:
     return cost
 
 
-def calculate_return_docs() -> float:
+def calculate_return_docs() -> Decimal:
     """
     Расчет стоимости возврата документов"""
     const = Const.info()
     return const.return_docs_cost
 
 
-def calculate_insurance(declared_cost: float) -> float:
+def calculate_insurance(declared_cost: Decimal) -> Decimal:
     """
     Расчет стоимости трахования груза"""
     const = Const.info()
     return declared_cost * (const.insurance_cost / 100)
 
 
-def calculate_prr(weight: float) -> float:
+def calculate_prr(weight: Decimal) -> Decimal:
     """
     Услуга ПРР"""
     const = Const.info()
     return weight * const.prr_cost
 
 
-def calculate_hard_packaging(volume: float) -> float:
+def calculate_hard_packaging(volume: Decimal) -> Decimal:
     """
     Расчет стоимости жесткой упаковки/обрешетки"""
     const = Const.info()
@@ -190,16 +196,16 @@ def calculate_hard_packaging(volume: float) -> float:
     return cost
 
 
-def calculate_base_cost(order: dict) -> float:
-    total_weight = float(sum([item.get("weight", 0) for item in order["items"]]))
-    total_volume = float(sum([item.get("volume", 0) for item in order["items"]]))
+def calculate_base_cost(order: dict) -> Decimal:
+    total_weight = (sum([item.get("weight", 0) for item in order["items"]]))
+    total_volume = (sum([item.get("volume", 0) for item in order["items"]]))
     city_from = City.find_by_id(order.get("city_from_id"))
     city_to = City.find_by_id(order.get("city_to_id"))
     rate = rate_by_cities(city_from, city_to)
     return calculate_delivery_cost_by_rate(rate, total_weight, total_volume)
 
 
-def calculate_order(order: dict) -> float:
+def calculate_order(order: dict) -> Decimal:
     noon = time(12, 0, 0)
     cost = calculate_base_cost(order)
     if order.get("from_address", False):
@@ -209,17 +215,17 @@ def calculate_order(order: dict) -> float:
     if order.get("return_docs", False):
         cost += calculate_return_docs()
     if order.get("insurance", False):
-        cost += calculate_insurance(float(order.get("declared_cost", 0)))
+        cost += calculate_insurance((order.get("declared_cost", 0)))
     for item in order["items"]:
         if item.get("prr_from", False):
-            cost += calculate_prr(float(item.get("weight", 0)))
+            cost += calculate_prr((item.get("weight", 0)))
     for item in order["items"]:
         if item.get("prr_to", False):
-            cost += calculate_prr(float(item.get("weight", 0)))
+            cost += calculate_prr((item.get("weight", 0)))
         if item.get("hard_packaging", False):
-            cost += calculate_hard_packaging(float(item.get("volume", 0)))
+            cost += calculate_hard_packaging((item.get("volume", 0)))
     return cost
 
 
-def calculate_item_order(item_order: dict) -> float:
+def calculate_item_order(item_order: dict) -> Decimal:
     pass
--- calculator_app/tests.py
+++ calculator_app/tests.py
@@ -1,3 +1,4 @@
+from decimal import Decimal
 from django.test import TestCase
 from index_app.models import Const
 from calculator_app.models import (
@@ -9,7 +10,7 @@ from calculator_app.services import (
     calculate_delivery_cost_by_rate,
     calculate_delivery_cost,
     rate_by_cities,
-    calculate_nigth_delevery
+    calculate_delevery_on_time
 )
 from datetime import time
 
@@ -21,8 +22,8 @@ class RateTestCase(TestCase):
         self.belgorod = City.objects.create(name="Белгород")
         self.rate = Rate.objects.create(
             city_from=self.moskow, city_to=self.belgorod,
-            cost_by_weight_0_25=900)
-        Const.objects.create(nigth_deliver_cost=1000)
+            cost_by_weight_0_25=Decimal("900"))
+        Const.objects.create(nigth_deliver_cost=Decimal("1000"))
 
     def test_find_moskow(self):
         moskow = city_by_name("Москва")
@@ -36,16 +37,16 @@ class RateTestCase(TestCase):
         # Москва-Белгород
         self.assertEqual(calculate_delivery_cost_by_rate(
             self.rate,
-            weight=15), 900)
+            weight=Decimal("15")), Decimal("900"))
 
     def test_calculate_delivery_cost(self):
         # Москва-Белгород
         self.assertEqual(calculate_delivery_cost(
             self.moskow.name,
             self.belgorod.name,
-            weight=15), 900)
+            weight=Decimal("15")), Decimal("900"))
 
-    def test_calculate_nigth_delevery(self):
-        self.assertEqual(calculate_nigth_delevery(
-            deliver_time=time(20, 0, 0)
-            ), 1000)
+    def test_calculate_delevery_on_time(self):
+        self.assertEqual(calculate_delevery_on_time(
+            deliver_time=time(22, 0, 0)
+            ), Decimal("1000"))
