45e3f831ffdb816b670cec6144fb94a2be095ac8
denis sergeevich pekshev <pekshev.ds@gmail.com>
2024-07-16 07:11:19 +0300

    add requests

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -1,9 +1,12 @@
 import os
 import sys
-
-
-sys.path.insert(0, '/var/www/u0872810/data/www/klaringl.annasoft.site/server')
-sys.path.insert(1, '/var/www/u0872810/data/www/klaringl.annasoft.site/.venv/lib/python3.10/site-packages')
-os.environ['DJANGO_SETTINGS_MODULE'] = 'server.settings'
 from django.core.wsgi import get_wsgi_application
+
+sys.path.insert(0, "/var/www/u0872810/data/www/klaringl.annasoft.site/server")
+sys.path.insert(
+    1,
+    "/var/www/u0872810/data/www/klaringl.annasoft.site/.venv/lib/python3.10/site-packages",
+)
+os.environ["DJANGO_SETTINGS_MODULE"] = "server.settings"
+
 application = get_wsgi_application()
