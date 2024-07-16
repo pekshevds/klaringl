d06015d50ee4d2011369410609df1eb06c33102c
MaximPekshev <info@annasoft.ru>
2024-07-16 14:46:11 +0300

    refactoring

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -1,12 +1,7 @@
-import os
-import sys
+# -*- coding: utf-8 -*-
+import os, sys
+sys.path.insert(0, '/var/www/u0872810/data/www/klaringl.annasoft.site/server')
+sys.path.insert(1, '/var/www/u0872810/data/www/klaringl.annasoft.site/.venv/lib/python3.10/site-packages')
+os.environ['DJANGO_SETTINGS_MODULE'] = 'server.settings'
 from django.core.wsgi import get_wsgi_application
-
-sys.path.insert(0, "/var/www/u0872810/data/www/klaringl.annasoft.site/server")
-sys.path.insert(
-    1,
-    "/var/www/u0872810/data/www/klaringl.annasoft.site/.venv/lib/python3.10/site-packages",
-)
-os.environ["DJANGO_SETTINGS_MODULE"] = "server.settings"
-
 application = get_wsgi_application()
